ebaac187151c8ed349f81b169d7f40bae0321694
vijaykrishnaapsgst <vijaykrishnaa@psgsoftwaretechnologies.com>
2021-07-01 17:02:08 +0530

    solves exercise 10

--- app/app.js
+++ app/app.js
@@ -7,8 +7,83 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    $routeProvider.otherwise({ redirectTo: '/view1' });
+  }])
+
+  .controller('MainController', function ($scope) {
+    $scope.NavigateExercises = true;
+
+    function loadExercise() {
+      $scope.exercises = {
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false,
+        6: false,
+        7: false,
+        8: false,
+        9: false,
+        10: false,
+      };
+    }
+    loadExercise();
+
+    $scope.goToExercise = function (key) {
+      loadExercise();
+      $scope.NavigateExercises = false;
+      $scope.exerciseView = true;
+      $scope.exercises[key] = true;
+      $scope.activeExercise = key;
+    }
+
+    $scope.goToNext = function (currentKey) {
+      $scope.goToExercise(parseInt(currentKey) + 1);
+    }
+
+    $scope.showNext = function () {
+      return $scope.activeExercise != Object.keys($scope.exercises).length;
+    }
+
+    $scope.goToPrevious = function (currentKey) {
+      $scope.goToExercise(parseInt(currentKey) - 1);
+    }
+
+    $scope.showPrevious = function () {
+      return $scope.activeExercise != 1;
+    }
+
+    $scope.goBack = function () {
+      $scope.NavigateExercises = true;
+      $scope.exerciseView = false;
+      delete $scope.activeExercise;
+      loadExercise();
+    }
+  })
+
+  .directive('exerciseView', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: 'app/partials/exerciseView.html',
+      controller: 'View1Ctrl'
+    }
+  })
+
+  .directive('exerciseQuestion', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: 'app/partials/exerciseQuestion.html',
+      controller: 'View1Ctrl'
+    }
+  })
+
+  .directive('exerciseSolution', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: 'app/partials/exerciseSolutions.html',
+      controller: 'View1Ctrl'
+    }
+  });
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,61 +3,13 @@
 angular.module('myApp.view1', ['ngRoute'])
 
   .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/view1', {
+    $routeProvider.when('/', {
       templateUrl: 'app/view1/view1.html',
       controller: 'View1Ctrl'
     });
   }])
 
   .controller('View1Ctrl', function ($scope) {
-    $scope.NavigateExercises = true;
-
-    function loadExercise() {
-      $scope.exercises = {
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-        5: false,
-        6: false,
-        7: false,
-        8: false,
-        9: false,
-        10: false,
-      };
-    }
-    loadExercise();
-
-    $scope.goToExercise = function (key) {
-      loadExercise();
-      $scope.NavigateExercises = false;
-      $scope.exerciseView = true;
-      $scope.exercises[key] = true;
-      $scope.activeExercise = key;
-    }
-
-    $scope.goToNext = function (currentKey) {
-      $scope.goToExercise(parseInt(currentKey) + 1);
-    }
-
-    $scope.showNext = function () {
-      return $scope.activeExercise != Object.keys($scope.exercises).length;
-    }
-
-    $scope.goToPrevious = function (currentKey) {
-      $scope.goToExercise(parseInt(currentKey) - 1);
-    }
-
-    $scope.showPrevious = function () {
-      return $scope.activeExercise != 1;
-    }
-
-    $scope.goBack = function () {
-      $scope.NavigateExercises = true;
-      $scope.exerciseView = false;
-      delete $scope.activeExercise;
-      loadExercise();
-    }
 
     // Exercise 3 
     function getCurrentWeekNumber(currentYear) {
@@ -127,6 +79,14 @@ angular.module('myApp.view1', ['ngRoute'])
                            the only player to have scored one hundred international centuries.`;
   })
 
+  .controller('calculatorController', function ($scope) {
+    // Exercise 8
+    $scope.setNumbers = function () {
+      $scope.number1 = 0;
+      $scope.number2 = 0;
+    }
+  })
+
   .controller('profileController1', function ($scope) {
     // Exercise 9
     $scope.profileName2 = "Sachin Tendulkar";
@@ -153,34 +113,26 @@ angular.module('myApp.view1', ['ngRoute'])
     $scope.description2 = "";
   })
 
-  .controller('calculatorController', function ($scope) {
-    // Exercise 8
-    $scope.setNumbers = function () {
-      $scope.number1 = 0;
-      $scope.number2 = 0;
-    }
+  .controller('profileController4', function ($scope) {
+    // Exercise 10
+    $scope.profile = {
+      name: "Sachin Tendulkar",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Sachin_Tendulkar_at_MRF_Promotion_Event.jpg/260px-Sachin_Tendulkar_at_MRF_Promotion_Event.jpg",
+      description: `Sachin Ramesh Tendulkar is an Indian former international cricketer who served as captain of the Indian national team.
+      He is widely regarded as one of the greatest batsmen in the history of cricket. He is the highest run scorer of all time in international cricket, and 
+      the only player to have scored one hundred international centuries.`
+    };
   })
 
-  .directive('exerciseView', function () {
-    return {
-      restrict: 'EA',
-      templateUrl: 'app/partials/exerciseView.html',
-      controller: 'View1Ctrl'
-    }
+  .controller('profileController5', function ($scope) {
+    // Exercise 10
+    $scope.profile = {
+      name: "Sachin Tendulkar",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Sachin_Tendulkar_at_MRF_Promotion_Event.jpg/260px-Sachin_Tendulkar_at_MRF_Promotion_Event.jpg",
+      description: `Sachin Ramesh Tendulkar is an Indian former international cricketer who served as captain of the Indian national team.
+      He is widely regarded as one of the greatest batsmen in the history of cricket. He is the highest run scorer of all time in international cricket, and 
+      the only player to have scored one hundred international centuries.`
+    };
   })
 
-  .directive('exerciseQuestion', function () {
-    return {
-      restrict: 'EA',
-      templateUrl: 'app/partials/exerciseQuestion.html',
-      controller: 'View1Ctrl'
-    }
-  })
-
-  .directive('exerciseSolution', function () {
-    return {
-      restrict: 'EA',
-      templateUrl: 'app/partials/exerciseSolutions.html',
-      controller: 'View1Ctrl'
-    }
-  });
+  
